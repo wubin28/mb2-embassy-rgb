384731d72fe8630cf0c86c34bb9b4971c0f62ecf
Bart Massey <bart.massey@gmail.com>
2024-02-28 01:22:40 -0800

    got defmt working

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 #![no_std]
 #![no_main]
 
-//use defmt::*;
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::{
+    gpio::{Level, Output, OutputDrive},
+    peripherals::{P0_21, P0_28},
+};
 use embassy_time::{Duration, Timer};
-//use defmt_rtt as _;
+use defmt_rtt as _;
 use panic_probe as _;
 
 #[embassy_executor::task]
 async fn blinker(
-    _col1: Output<'static>,
-    mut row1: Output<'static>,
+    _col1: Output<'static, P0_28>,
+    mut row1: Output<'static, P0_21>,
     interval: Duration,
 ) -> ! {
     loop {
         row1.set_high();
+        println!("high");
         Timer::after(interval).await;
         row1.set_low();
+        println!("low");
         Timer::after(interval).await;
     }
 }
@@ -28,5 +33,6 @@ async fn main(spawner: Spawner) {
     let col1 = Output::new(p.P0_28, Level::Low, OutputDrive::Standard);
     let row1 = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
 
-    let _ = spawner.spawn(blinker(col1, row1, Duration::from_millis(500)));
+    println!("spawning");
+    unwrap!(spawner.spawn(blinker(col1, row1, Duration::from_millis(500))));
 }
